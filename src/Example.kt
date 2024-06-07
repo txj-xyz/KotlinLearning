c07934cc6222d1d68d61a651343edb19dd04a247
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-06-07 10:46:56 -0600

    feat: Lesson 1-3 note: Hello World and project setup

---
+++ src/Example.kt
@@ -0,0 +1,9 @@
+/*
+    Main function
+    Functions are declared via `fun`
+    `main` is required for entrypoint similar to GoLang
+ */
+fun main() {
+    println("Hello")
+    println(" World!")
+}
