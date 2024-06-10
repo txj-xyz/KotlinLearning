28332ab3abdff22f253fc0fd6440c80866bf62e7
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-06-10 14:59:42 -0600

    lesson-6: functions lesson completed

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,5 +1,34 @@
 /*
     Lesson 6
     Functions
+    NOTES:
+    Unit = void (nothing in TS/JS)
+    fun nameOfFunction(variable: Type): ReturnType {}
+    Live Templates:
+        fun0 = function with 0 params
+        fun1 = function with 1 params
+        fun2 = function with 2 params
+        TAB to skip through the fills
  */
 
+fun main() {
+    //     result1 = Unit return, aka Void / Nothing
+    val result = sumOfTwoNumbers2(3, 7)
+    println(result)
+}
+
+
+// Block body function with no params
+fun sumOfTwoNumbers() {
+    println(2 + 2)
+}
+
+// Block body function with parameters
+fun sumOfTwoNumbers(a: Int, b: Int): Int {
+    // (this is a block body function)
+    // return println() { DOESN'T WORK BECAUSE PRINTLN IS VOID OR UNIT }
+    return a + b
+}
+
+// Expression body function (= syntax)
+fun sumOfTwoNumbers2(a: Int, b: Int) = a + b
