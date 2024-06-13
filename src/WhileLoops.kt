5296b0a63f1616182ca9e08d06e3532c712652e8
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-06-13 15:55:39 -0600

    lesson-10-feat: do while loops and challenge homework

--- src/WhileLoops.kt
+++ src/WhileLoops.kt
@@ -4,11 +4,32 @@
         NOTES: Should always return a true or a false in a `while()` similar to JS/TS `while -> expression is true -> do`
  */
 
+//fun main() {
+//    var number = 0
+//    while (conditionExpressionBody(number = number)) {
+//        println(number)
+//        // number++ = add
+//        // number-- = subtract
+//        // number -= 10 = remove 10 from `number`
+//        number += 10
+//    }
+//}
+
+
+//fun conditionBlockBody(number: Int): Boolean {
+//    return number <= 10
+//}
+
+// Alternative version of a `while()` except `do(){}while(){}` means it will at minimum execute one time
+// FUN CHALLENGE ALSO I WIN SIT EASY KID LMAOOOOO
 fun main() {
-    var number = 0
-    while (number <= 10) {
-        println(number)
-        number++ // add
-        // number-- = subtract
-    }
+    val myWord = "Hello World"
+    var i = 0
+    do {
+        println(myWord[i])
+        i++
+    } while (i < myWord.length)
 }
+
+
+//fun conditionExpressionBody(number: Int) = number <= 100
