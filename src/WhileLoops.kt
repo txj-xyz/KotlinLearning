a0fcddf50fd2a6712cc017805c3acf90bed6819b
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-06-13 15:23:39 -0600

    feat: Lesson 10 while loops and execution

---
+++ src/WhileLoops.kt
@@ -0,0 +1,14 @@
+/*
+    Lesson 10
+    While loops
+        NOTES: Should always return a true or a false in a `while()` similar to JS/TS `while -> expression is true -> do`
+ */
+
+fun main() {
+    var number = 0
+    while (number <= 10) {
+        println(number)
+        number++ // add
+        // number-- = subtract
+    }
+}
