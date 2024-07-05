f81ffea5777d56bcbb0d6b2526d1a45228d68a78
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2024-07-05 15:52:00 -0600

    feat: main entry point file so git has something to run as a main(){ ... }

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("""
+        --- Kotlin Learning ---
+        This repo is for learning
+        the Java/Kotlin language
+        theres completed lessons
+        in each folder for the
+        course I took online
+        -----------------------
+    """.trimIndent())
+}
